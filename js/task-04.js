58671c55c22adc5833dc6b1be549710a01bc75d3
JulianskyMe <juliakamynskaya@gmail.com>
2023-12-09 15:27:43 +0200

    commit

--- js/task-04.js
+++ js/task-04.js
@@ -1,11 +1,9 @@
   let counterValue = 0;
 
-  // Знаходимо необхідні елементи DOM
   const decrementButton = document.querySelector('[data-action="decrement"]');
   const incrementButton = document.querySelector('[data-action="increment"]');
   const value = document.getElementById('value');
 
-  // Додаємо слухачів подій для кнопок
   decrementButton.addEventListener('click', () => {
     counterValue -= 1;
     value.textContent = counterValue;
